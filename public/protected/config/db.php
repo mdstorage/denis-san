c5f22828b4547b101ff97df8c83e298430ea3696
denis <d@d.d>
2014-12-22 19:42:29 +0300

    третий коммит

---
+++ public/protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+$db = array(
+    'connectionString' => 'mysql:host=localhost;dbname=nissan',
+    'emulatePrepare' => true,
+    'username' => 'nissan',
+    'password' => 'Bmww4YV8Vcc9Sx8W',
+    'charset' => 'utf8',
+);
+?>
